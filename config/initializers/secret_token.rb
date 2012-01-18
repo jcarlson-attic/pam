1e006e14c48e78d9f1f323716683355d53fc616f
Jarrod Carlson <jcarlson@Eosphorus.local>
2012-01-18 18:04:15 -0500

    Initial code commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pam::Application.config.secret_token = 'd65e7dcec2ade37e0316e2decbcb729283263c2b916f4c1dfd52625c908e2812b08ae5337fbd3ee5fed1cabe849f1d4019084beec57ff09fbf56b0cce5c8095d'
